05709a13c0813f07b870ca3f1854852d672cebda
Николай Попков <xaoc.py@gmail.com>
2017-12-09 15:11:24 +0300

    добавил функции по коментариям

--- js/pictures.js
+++ js/pictures.js
@@ -1,13 +1,17 @@
 'use strict';
 
+
+var pictures = document.querySelector('.pictures');
+var fragment = document.createDocumentFragment();
+
 var comments = [
   'Всё отлично!',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-var getRandomComment = function () {
-  return Math.round(Math.random() * (comments.length));
+var getRandomInteger = function () {
+  return Math.floor(Math.random() * (comments.length));
 };
 
 var getRandomLike = function (min, max) {
@@ -19,14 +23,14 @@ for (var i = 1; i <= 25; i++) {
   var photoArray = {
     url: 'photos/' + [i] + '.jpg',
     likes: getRandomLike(15, 200),
-    comments: getRandomComment()
+    comments: getRandomInteger()
   };
   photos.push(photoArray);
 }
 
 
-var pictureTemplate = document.querySelector('#picture-template').content.querySelector('.picture');
 var getRandomPicture = function (img) {
+  var pictureTemplate = document.querySelector('#picture-template').content.querySelector('.picture');
   var pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('img').src = img.url;
   pictureElement.querySelector('.picture-likes').textContent = img.likes;
@@ -34,17 +38,20 @@ var getRandomPicture = function (img) {
   return pictureElement;
 };
 
-var pictures = document.querySelector('.pictures');
-var fragment = document.createDocumentFragment();
-for (var j = 0; j < photos.length; j++) {
-  fragment.appendChild(getRandomPicture(photos[j]));
-}
+
+var getPictureFragment = function () {
+  for (var j = 0; j < photos.length; j++) {
+    fragment.appendChild(getRandomPicture(photos[j]));
+  }
+  return fragment;
+};
+getPictureFragment();
 pictures.appendChild(fragment);
 
 
-var galleryOverlay = document.querySelector('.gallery-overlay');
-galleryOverlay.classList.remove('hidden');
 var renderGalleryOverlay = function (photo) {
+  var galleryOverlay = document.querySelector('.gallery-overlay');
+  galleryOverlay.classList.remove('hidden');
   var galleryOverlayImg = document.querySelector('.gallery-overlay-image');
   galleryOverlayImg.setAttribute('src', photo.url);
   galleryOverlay.querySelector('.likes-count').textContent = photo.likes;
